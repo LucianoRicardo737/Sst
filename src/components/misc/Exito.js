b8306d38f12862d973f99a7832f655569420ab07
root <lucianoricardo737@gmail.com>
2020-12-12 00:54:08 -0300

    una cosita mas jajaja

--- src/components/misc/Exito.js
+++ src/components/misc/Exito.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './exito.css'
 export default function ErrorUsuarios(props){
     return (
-        <div className='error-notice animate__animated animate__headShake '>
+        <div className='error-notice2 animate__animated animate__headShake '>
             <span className='errorMessage'>{props.message}</span>
             <button className='closeErrorButton' onClick={props.clearError}>X</button>
         </div>
